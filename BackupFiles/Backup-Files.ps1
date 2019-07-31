bc95bb74cbfffcd00b38e2ecb114e16567a61182
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-07-31 09:10:42 -0400

    Update Backup-Files.ps1
    Fixed a few typos

--- BackupFiles/Backup-Files.ps1
+++ BackupFiles/Backup-Files.ps1
@@ -85,7 +85,7 @@ param (
 	[switch]$Orienteering,
 	[switch]$FLL,
 	[switch]$MARS,
-	[switch]$GitHub
+	[switch]$GitHub,
 	[switch]$AllPowerShell #Backs up all PoweShell files instead of just ps1
 	#[switch]$Anaconda3
 	)
@@ -125,7 +125,7 @@ if ($Zotero -OR $CodeSamples -OR $NuclearScience -OR $Orienteering -OR $MachineL
 	if ($FLL) { $DirectoriesToBackup += "C:\Users\jbrus\Google Drive\Documents\2018-FLL" }
 	if ($AllPowerShell) { $DirectoriesToBackup += "C:\Users\jbrus\Google Drive\Documents\ProgrammingClasses\CodeSamples\PowerShell" }
 	if ($MARS) { $DirectoriesToBackup += "C:\Users\jbrus\Google Drive\Documents\MARS" }
-	if ($GitHub) { $DirectoriesToBackup += "C:\Users\jbrus\Documents\GitHub"
+	if ($GitHub) { $DirectoriesToBackup += "C:\Users\jbrus\Documents\GitHub" }
 }
 else
 {
