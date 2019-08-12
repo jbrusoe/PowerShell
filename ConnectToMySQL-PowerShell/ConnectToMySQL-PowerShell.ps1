333c2b7176c90d4da9649f9471478088c512ccb3
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-08-12 12:20:20 -0400

    Added MySQL connecdtion file demo

---
+++ ConnectToMySQL-PowerShell/ConnectToMySQL-PowerShell.ps1
@@ -0,0 +1,47 @@
+#Connect-ToMySQL.ps1
+#Written by: Jeff Brusoe
+#
+#Purpose: This file is intended to connect to MySQL with MS PowerShell.
+
+[void][system.reflection.Assembly]::LoadWithPartialName("MySql.Data")
+
+[string]$MySQLHost = "localhost"
+[string]$User="peruser3"
+[string]$Password = "MyPassword"
+[string]$Database = "per"
+
+$ConnStr = "server=" + $MySQLHost + ";uid=" + $User + ";pwd=" + $Password + ";database=" + $Database + ";"
+Write-Output "Connection String: $connStr"
+
+$MySQLConn = New-Object MySql.Data.MySqlClient.MySqlConnection($ConnStr)
+$MySqlConn.Open()
+
+$query = "select * from per.co2;"
+
+$cmd = $MySQLConn.CreateCommand() 
+$cmd.CommandText = $query 
+
+$reader = $cmd.ExecuteReader()
+
+[bool]$PrintColumnTitles = $true
+
+while ($reader.Read()) 
+{
+	if ($PrintColumnTitles)
+	{
+		for ([int]$Column = 0; $Column -lt $reader.FieldCount; $Column++)
+		{
+			Write-Host $($reader.GetName($Column) + "`t") -ForeGroundColor Green -NoNewLine
+		}
+
+		Write-Host ""
+		$PrintColumnTitles = $false;
+	}
+	
+	for ($i= 0; $i -lt $reader.FieldCount; $i++) 
+	{
+		Write-Host $($reader.GetValue($i).ToString() + "`t") -NoNewLine
+	}
+	
+	Write-Host ""
+}
