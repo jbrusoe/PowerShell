45b0969560ed6dbcd985b43cf86e493c09afd93d
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-08-19 07:42:44 -0400

    Changed Get-DriveInfo files

--- 3AdminTasks/Get-DiskInfo.ps1
+++
@@ -1,5 +0,0 @@
-$localVolumes = Get-WMIObject win32_volume | where {$_.DriveLetter -ne $null}
-
-$LocalVolumes | select DriveLetter,
-	@{Name="Free(GB)";Expression={[math]::round($_.FreeSpace/1GB, 2)}},
-	@{Name="Used(GB)";Expression={[math]::round(($_.Capacity-$_.FreeSpace)/1GB, 2)}}
