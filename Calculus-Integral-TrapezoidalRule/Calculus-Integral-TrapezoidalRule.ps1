84424aca18efae21f32c3ad1145c962af3290d1d
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-08-23 08:08:10 -0400

    Update Calculus-Integral-TrapezoidalRule.ps1

--- Calculus-Integral-TrapezoidalRule/Calculus-Integral-TrapezoidalRule.ps1
+++ Calculus-Integral-TrapezoidalRule/Calculus-Integral-TrapezoidalRule.ps1
@@ -5,20 +5,29 @@ Write-Output "Number of intervals: $NumberOfIntervals"
 
 $b = 5
 $a = 2
-$Deltax = ($b-$a)/(2*$NumberOfIntervals)
+$Deltax = ($b-$a)/$NumberOfIntervals
 
 Write-Output "Delta x: $Deltax"
 
 $Area = 0
 
-for ($x = 0; $x -le $NumberOfIntervals; $x++)
+for ($i = 0; $i -le $NumberOfIntervals; $i++)
 {
+	$x = $a + ($i*$Deltax)
+	Write-Output "Current Value of X: $x"
+	
 	if (($x -eq 0) -OR ($x -eq $NumberOfIntervals))
 	{
-	
+		$Area = $Area + ([math]::Pow($x,4)/8)
 	}
 	else
 	{
-	
+		$Area = $Area + ([math]::Pow($x,4)/4)
 	}
 }
+
+$Area = $Area * $Deltax
+
+Write-Output "Answer from Trapezoidal Rule: $Area"
+Write-Output "Actual Answer: 152.25"
+Write-Output "Since the second derivative of x^3 is positive, we'd expect the Trapezoidal Rule to overestimate the actual answer."
