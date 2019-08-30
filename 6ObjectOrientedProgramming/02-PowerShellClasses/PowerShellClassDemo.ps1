8201ee60bd747e4e4c724044428b0f105cc0b01f
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-08-29 23:19:32 -0400

    Began to work on a demo file of PowerShell classes

--- 6ObjectOrientedProgramming/02-PowerShellClasses/PowerShellClassDemo.ps1
+++ 6ObjectOrientedProgramming/02-PowerShellClasses/PowerShellClassDemo.ps1
@@ -1,1 +1,47 @@
-#This is a test.
+#PowerShell Classes Demo
+#Written by: Jeff Brusoe
+#Last Updated: August 29, 2019
+#
+#Note: Requires Version 5 or greater.
+#https://overpoweredshell.com/Introduction-to-PowerShell-Classes/
+
+class Book {
+	[string]$Title
+	[string]$AuthorFirstName
+	[string]$AuthorLastName
+	[bool]$Read
+	[int]$NumberOfPages
+	[bool]$RequiredForClass
+
+	[Guid]hidden $GUID = (New-Guid).Guid
+
+	[void]TestMethod()
+	{
+		return
+	}
+	
+	[string]GetTitle()
+	{
+		return $this.Title
+	}
+}
+
+$MyBook = [Book]::new()
+$MyBook
+$MyBook.Title = "Rendezvous with Rama"
+
+Write-Output "Piped to Get-Member"
+$MyBook | Get-Member -MemberType Properties
+
+Write-Output "Piped to Get-Member with -Force switch"
+$MyBook | Get-Member -MemberType Properties -Force
+
+#Note: Guid can be accessed this way even though it is hidden
+$MyBook.Guid
+
+Write-Output "Calling test method"
+$MyBook.TestMethod()
+
+Write-Output "Calling GetTitle method"
+$MyBook.GetTitle()
+
