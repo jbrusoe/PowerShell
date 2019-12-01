39c698f8d7464e3029ee5c5591e73f6232452495
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2019-12-01 10:31:58 -0500

    Update Start-LabVIEWService.ps1

--- 3AdminTasks/Start-LabVIEWService/Start-LabVIEWService.ps1
+++ 3AdminTasks/Start-LabVIEWService/Start-LabVIEWService.ps1
@@ -1,32 +1,45 @@
 param (
-	[switch]$StopServices
+	[switch]$StopServices,
+	[switch]$Testing
 	)
 
-$services = "lkClassAds","lkTimeSync","mxssvr","NIApplicationWebServer","niauth","NIDomainService","niLXIDiscovery","nimDNSResponder","NINetworkDiscovery","nipxicmsvc","nipxirmu","niroco","NiSvcLoc","NISystemWebServer","NITaggerService"
+#Environment configuration
+Clear-Host
+$Error.Clear()
 
-foreach ($service in $services)
-{
-	$Error.Clear()
-	
-	Write-Output "Service Name: $service"
+$TranscriptLog = $PSScriptRoot + "\Logs\" + (Get-Date -Format yyyy-MM-dd-HH-mm) + "-SessionTranscript.txt"
+Write-Verbose "Transcript Log File: $TranscriptLog"
+
+Start-Transcript $TranscriptLog
+#End of environment configuration
+
+$LVServices = "lkClassAds","lkTimeSync","mxssvr","NIApplicationWebServer","niauth","NIDomainService","niLXIDiscovery","nimDNSResponder","NINetworkDiscovery","nipxicmsvc","nipxirmu","niroco","NiSvcLoc","NISystemWebServer","NITaggerService"
+
+foreach ($LVService in $LVServices)
+{	
+	Write-Output "Service Name: $LVService"
 	
 	try
 	{
 		if (!$StopServices)
 		{
-			Get-Service $service | Start-Service -ea "Stop"
+			Get-Service $LVService | Start-Service -ea "Stop"
 			Write-Output "Successfully started service"
 		}
 		else
 		{
-			Get-Service $service | Stop-Service -ea "Stop" -Force
+			Get-Service $LVService | Stop-Service -ea "Stop" -Force
 			Write-Output "Successfully stopped service"
 		}
 	}
 	catch
 	{
 		Write-Warning "Error starting/stopping service"
-		return
+		
+		if ($Testing)
+		{
+			return
+		}
 	}
 	
 	Write-Output "*******************"
