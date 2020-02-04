9adb4f53b30ddf4bf9c27d1ab62a3da4b94e024d
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-02-04 11:43:52 -0500

    Update Backup-Files.ps1

--- 4HomeworkFiles/BackupFiles/Backup-Files.ps1
+++ 4HomeworkFiles/BackupFiles/Backup-Files.ps1
@@ -128,7 +128,7 @@ if ($Zotero -OR $CodeSamples -OR $NuclearScience -OR $Orienteering -OR $MachineL
 	if ($AllPowerShell) { $DirectoriesToBackup += "C:\Users\jbrus\Google Drive\Documents\ProgrammingClasses\CodeSamples\PowerShell" }
 	if ($MARS) { $DirectoriesToBackup += "C:\Users\jbrus\Google Drive\Documents\MARS" }
 	if ($GitHub) { $DirectoriesToBackup += "C:\Users\jbrus\Documents\GitHub" ; $DirectoriesToBackup += "C:\MARSGitHubRepo" }
-	if ($MARSGitHub) { $DirectoriesToBackup += "C:\MARSGitHubRepo" ; $DirectoriesToBackup += "C:\Users\jbrus\Documents\GitHub\MARS-2020" }
+	if ($MARSGitHub) { $DirectoriesToBackup += "C:\MARSGitHubRepo\MARS-Tucker-Team" ; $DirectoriesToBackup += "C:\Users\jbrus\Documents\GitHub\MARS-2020" }
 	if ($LabVIEW) { $DirectoriesToBackup += $DirectoriesToBackup += "C:\Users\jbrus\Documents\GitHub\LabVIEW" }
 	
 }
