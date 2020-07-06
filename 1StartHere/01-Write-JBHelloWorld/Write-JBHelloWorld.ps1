2648c600769d4d2bba01a3750071ff55ea10e028
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-07-05 21:38:25 -0400

    File and directory renames

---
+++ 1StartHere/01-Write-JBHelloWorld/Write-JBHelloWorld.ps1
@@ -0,0 +1,39 @@
+#PowerShell "Hello World" demonstration
+#
+#Written by: Jeff Brusoe
+#Date: June 17, 2017
+#
+#There are many ways to write output in PowerShell, and a few different ways are shown here.
+#Some are better than others and depend on the situation.
+
+Write-Output "Method 1: Just using quotes -`"Hello World!`""
+"Hello World!`n"
+
+#OR
+
+Write-Output "Method 2: Using Write-Host"
+Write-Host "Hello World!`n"
+
+#OR
+
+Write-Output "Method 3: Using Write-Host with -ForegroundColor paramter"
+Write-Host "Hello World!`n" -ForegroundColor Cyan
+
+#OR
+
+Write-Output "Method 4: Using Write-Output"
+Write-Output "Hello World!`n"
+
+#OR
+
+Write-Output "Method 5: Using the echo alias"
+echo "Hello World!`n"
+
+#OR
+Write-Output "Method 6: Using the Write alias"
+Write "Hello World!`n"
+
+#OR
+Write-Output "Method 7: Using Write-Warning"
+Write-Warning "Hello World!`n"
+
