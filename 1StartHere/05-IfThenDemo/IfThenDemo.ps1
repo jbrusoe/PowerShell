4c68ab8dd12090eb162ec8c5cbff37f5f05bc75d
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-11-06 17:55:39 -0500

    Update IfThenDemo.ps1

--- 1StartHere/05-IfThenDemo/IfThenDemo.ps1
+++ 1StartHere/05-IfThenDemo/IfThenDemo.ps1
@@ -6,15 +6,12 @@
 [int]$Number1 = Read-Host "Enter a number"
 [int]$Number2 = Read-Host "Enter another number"
 
-if ($Number1 -eq $Number2)
-{
+if ($Number1 -eq $Number2) {
 	Write-Output "The two numbers are equal."
 }
-elseif ($Number1 -gt $Number2)
-{
+elseif ($Number1 -gt $Number2) {
 	Write-Output "$Number1 is greater than $Number2"
 }
-else
-{
+else {
 	Write-Output "$Number2 is greater than $Number1"
 }
