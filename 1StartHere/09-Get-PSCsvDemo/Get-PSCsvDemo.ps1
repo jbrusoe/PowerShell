397672367f2db2fd8d1954a7d0520d4deb62b1a4
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-12-02 19:53:54 -0500

    Update Get-PSCsvDemo.ps1

--- 1StartHere/09-Get-PSCsvDemo/Get-PSCsvDemo.ps1
+++ 1StartHere/09-Get-PSCsvDemo/Get-PSCsvDemo.ps1
@@ -4,10 +4,10 @@
 
 Write-Output "Generating a CSV File: ReadWriteCSV.csv"
 
-Get-Service | where {$_.Status -eq "Running"} | Export-CSV ReadWriteCSV.csv -Force #-Force overwrites the file if it already exists
+Get-Service | where {$_.Status -eq "Running"} |
+	Export-CSV ReadWriteCSV.csv -Force #-Force overwrites the file if it already exists
 
 Write-Output "CSV file generated"
-
 Write-Output "`nReading CSV File: ReadWriteCSV.csv"
 
 $Services = Import-Csv "ReadWriteCSV.csv"
