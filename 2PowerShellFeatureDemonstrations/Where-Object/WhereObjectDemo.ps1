37b63927ad66db6a26ea092975a5ac83525a1378
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-12-02 21:23:25 -0500

    Update WhereObjectDemo.ps1

--- 2PowerShellFeatureDemonstrations/Where-Object/WhereObjectDemo.ps1
+++ 2PowerShellFeatureDemonstrations/Where-Object/WhereObjectDemo.ps1
@@ -5,14 +5,10 @@ Get-Service | Where-Object {$_.Status -eq "Running"}
 
 #OR
 
-Start-Sleep -s 10
-
 Write-Output "`nwhere Alias`n"
 Get-Service | where {$_.Status -eq "Running"}
 
 #OR
-
-Start-Sleep -s 10
 
 Write-Output "`n? Alias`n"
 Get-Service | ? {$_.Status -eq "Running"}
