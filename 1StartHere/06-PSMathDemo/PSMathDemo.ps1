b53a6b62bf8f151570835bf1ad7dff985f8b877b
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-12-02 19:44:25 -0500

    Update PSMathDemo.ps1

--- 1StartHere/06-PSMathDemo/PSMathDemo.ps1
+++ 1StartHere/06-PSMathDemo/PSMathDemo.ps1
@@ -5,21 +5,24 @@
 #Written by: Jeff Brusoe
 #Last Updated: May 8, 2019
 
-#[float] forces the variables to become floating point numbers instead of strings (Read-Host default return type)
+Clear-Host
+
+#[float] forces the variables to become floating point numbers instead
+#of strings (Read-Host default return type)
 [float]$FirstNumber = Read-Host "Enter the first number"
 [float]$SecondNumber = Read-Host "Enter a second number"
 
-Write-Output $("$FirstNumber + $SecondNumber = " + $([float]$FirstNumber+[float]$SecondNumber))
-Write-Output $("$FirstNumber - $SecondNumber = " + $([float]$FirstNumber-[float]$SecondNumber))
-Write-Output $("$FirstNumber * $SecondNumber = " + $([float]$FirstNumber*[float]$SecondNumber))
+Write-Output $("$FirstNumber + $SecondNumber = " + $($FirstNumber+$SecondNumber))
+Write-Output $("$FirstNumber - $SecondNumber = " + $($FirstNumber-$SecondNumber))
+Write-Output $("$FirstNumber * $SecondNumber = " + $($FirstNumber*$SecondNumber))
 
-if ($SecondNumber -eq 0)
-{
+#Prevent division by 0
+if ($SecondNumber -eq 0) {
 	Write-Warning "Second number is 0. Unable to perform division operation"
 }
-else
-{
-	Write-Output $("$FirstNumber/$SecondNumber = " + $([math]::Round([float]$FirstNumber/[float]$SecondNumber,2)))
+else {
+	Write-Output $("$FirstNumber/$SecondNumber = " +
+		$([math]::Round([float]$FirstNumber/[float]$SecondNumber,2)))
 }
 
 Write-Output "`nFirstNumber: $FirstNumber"
@@ -27,7 +30,8 @@ Write-Output $("Absolute Value: " + [math]::abs($FirstNumber))
 
 if ($FirstNumber -lt 0)
 {
-	Write-Warning "You have entered a negative number. Unable to calculate its square root, log base 10, and natural logarithm"
+	Write-Warning $("You have entered a negative number. " +
+		"Unable to calculate its square root, log base 10, and natural logarithm")
 }
 elseif ($FirstNumber -eq 0)
 {
@@ -36,7 +40,9 @@ elseif ($FirstNumber -eq 0)
 }
 else
 {
-	Write-Host "You have entered a positive number. Calculating square root, log base 10, and natural logarithm" -ForegroundColor Green
+	Write-Host $("You have entered a positive number. Calculating square root, log base 10, " +
+		"and natural logarithm") -ForegroundColor Green
+
 	Write-Output $("Square Root: " + [math]::Round([math]::sqrt($FirstNumber),2))
 	Write-Output $("Log10: " + [math]::Round([math]::log10($FirstNumber)),2)
 	Write-Output $("Natural Log: " + [math]::Round([math]::log($FirstNumber),2))
