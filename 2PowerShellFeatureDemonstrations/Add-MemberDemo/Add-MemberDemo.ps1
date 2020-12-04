0a6f3aed674738f43a8aadb122c38bbc19d12151
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-12-03 19:38:03 -0500

    Update Add-MemberDemo.ps1

--- 2PowerShellFeatureDemonstrations/Add-MemberDemo/Add-MemberDemo.ps1
+++ 2PowerShellFeatureDemonstrations/Add-MemberDemo/Add-MemberDemo.ps1
@@ -1,17 +1,18 @@
 #Demonstration of PSObject
 
-$Book = New-Object -typename PSObject
-$Book | Add-Member -MemberType NoteProperty -Name "BookName" -Value "Intro Physics"
-$Book | Add-Member -MemberType NoteProperty -Name "Author" -Value "Halliday & Resnick"
-$Book | Add-Member -MemberType ScriptMethod -Name "GetBookInfo" -Value {"Title: " + $this.BookName + "`nAuthor: " + $this.Author}
+Clear-Host
 
-$Book
+$PhysicsBook = New-Object -TypeName PSObject
+$PhysicsBook | Add-Member -MemberType NoteProperty -Name "BookName" -Value "Intro Physics"
+$PhysicsBook | Add-Member -MemberType NoteProperty -Name "Author" -Value "Halliday & Resnick"
+$PhysicsBook | Add-Member -MemberType ScriptMethod -Name "GetBookInfo" -Value {"Title: " + $this.BookName + "`nAuthor: " + $this.Author}
+
+Write-Output `n'$PhysicsBook Object'
+$PhysicsBook
 
 Write-Output "`nCalling GetBookInfo method"
-$Book.GetBookInfo()
+$PhysicsBook.GetBookInfo()
 
 Write-Output "`nCalling objects individually"
-Write-Output $("Title: " + $Book.BookName)
-Write-Output $("Author: " + $Book.Author)
-
-
+Write-Output $("Title: " + $PhysicsBook.BookName)
+Write-Output $("Author: " + $PhysicsBook.Author)
