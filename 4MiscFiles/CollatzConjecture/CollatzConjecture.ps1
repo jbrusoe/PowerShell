2826eb4f86082340f4ad85b5ad052ebee1ba86d7
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-12-03 19:58:42 -0500

    Update CollatzConjecture.ps1

--- 4MiscFiles/CollatzConjecture/CollatzConjecture.ps1
+++ 4MiscFiles/CollatzConjecture/CollatzConjecture.ps1
@@ -1,15 +1,13 @@
+# Collatz Conjecture Demo in PowerShell
+# Written By: Jeff Brusoe
+# Last Updated: December 3, 2020
+
 <#
-Collatz Conjecture
-Written By: Jeff Brusoe
-
-Demonstration program for Programming Merit Badge class
-
 From Wikipedia - "The Collatz conjecture is a conjecture in mathematics that concerns a sequence 
 defined as follows: start with any positive integer n. Then each term is obtained from the previous
 term as follows: if the previous term is even, the next term is one half the previous term. If the 
 previous term is odd, the next term is 3 times the previous term plus 1. 
 The conjecture is that no matter what value of n, the sequence will always reach 1."
-
 #>
 
 [int]$Count = 0 #Keeps track of number of steps to get to 1
@@ -19,22 +17,16 @@ Write-Output "Original Number: $i"
 
 while (($Count -eq 0) -OR ($i -ne 1))
 {
-	if ($i%2 -eq 0)
-	{
+	if ($i%2 -eq 0) {
 		#Number is even
 		$i = $i/2
 	}
-	else
-	{
+	else {
 		#Number is odd
 		$i = 3*$i + 1
 	}
 	
 	$Count++
 	
-	Write-Output $("Sequence Term $Count" + ": $i")
+	Write-Output $("Sequence Term $Count`: $i")
 }
-
-
-
-
