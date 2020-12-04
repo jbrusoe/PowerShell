b9404a32d1343a75ff04e23e68bc82f482359b5f
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-12-03 19:48:21 -0500

    Update directory structure

--- 2PowerShellFeatureDemonstrations/Parameters/PowerShellParameters.ps1
+++
@@ -1,9 +0,0 @@
-#PowerShell - Command line parameters demonstration
-#Written by: Jeff Brusoe
-#Last Updated: October 16, 2018
-
-param (
-	[string]$FirstName = "Jenny"
-)
-
-Write-Output "FirstName: $FirstName"
