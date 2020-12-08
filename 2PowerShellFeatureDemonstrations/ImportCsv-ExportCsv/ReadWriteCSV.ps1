e030d0de79e2759fc282e9280320c68643db080a
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2020-12-08 00:29:15 -0500

    Update ReadWriteCSV.ps1

--- 2PowerShellFeatureDemonstrations/ImportCsv-ExportCsv/ReadWriteCSV.ps1
+++ 2PowerShellFeatureDemonstrations/ImportCsv-ExportCsv/ReadWriteCSV.ps1
@@ -4,7 +4,9 @@
 
 Write-Output "Generating a CSV File: ReadWriteCSV.csv"
 
-Get-Service | where {$_.Status -eq "Running"} | Export-CSV ReadWriteCSV.csv -Force #-Force overwrites the file if it already exists
+Get-Service |
+	where {$_.Status -eq "Running"} |
+	Export-CSV ReadWriteCSV.csv -Force
 
 Write-Output "CSV file generated"
 
@@ -12,7 +14,6 @@ Write-Output "`nReading CSV File: ReadWriteCSV.csv"
 
 $Services = Import-Csv "ReadWriteCSV.csv"
 
-foreach ($Service in $Services)
-{
+foreach ($Service in $Services) {
 	Write-Output $Service.Name
 }
