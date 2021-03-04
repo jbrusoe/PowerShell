f5bcbd9258fd1d0c55e5d49646233ae4af57c314
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2021-03-04 18:53:06 -0500

    Update WhatIfConfirm.ps1

--- 08AdvancedFunctions/08-WhatIfConfirm/WhatIfConfirm.ps1
+++ 08AdvancedFunctions/08-WhatIfConfirm/WhatIfConfirm.ps1
@@ -9,7 +9,7 @@ function Test-WhatIfConfirm
 	param()
 	
 	if ($PSCmdlet.ShouldProcess("Running Get Service")) {
-		Get-Service | Select-Object -First 10
+		Get-Service | Select-Object -First 3
 	}
 }
 
@@ -17,17 +17,43 @@ Write-Output "`n`nWithout -WhatIf"
 Write-Output "Without -Confirm"
 Test-WhatIfConfirm
 
-Write-Output "`n`nWith -WhatIf"
+Write-Output "`n***************************`n"
+
+Write-Output "`nWith -WhatIf"
 Write-Output "Without -Confirm"
 Test-WhatIfConfirm -WhatIf
 
-Write-Output "`n`nWithout -WhatIf"
+Write-Output "`n***************************`n"
+
+Write-Output "`nWithout -WhatIf"
+Write-Output "Without -Confirm"
+Write-Output "ConfirmPreference: $ConfirmPreference"
+Write-Output "ConfirmImpact: Medium"
+Test-WhatIfConfirm | Format-Table
+
+Write-Output "`n***************************`n"
+
+Write-Output "`nWithout -WhatIf"
 Write-Output "With -Confirm"
 Write-Output "ConfirmPreference: $ConfirmPreference"
 Write-Output "ConfirmImpact: Medium"
-Test-WhatIfConfirm -Confirm
+Test-WhatIfConfirm -Confirm | Format-Table
 
-Write-Output `n`n'Setting $ConfirmPreference to High'
-$ConfirmPreference = "High"
+Write-Output "`n***************************`n"
 
-Write-Output "`n`nWithout"
+Write-Output `n'Setting $ConfirmPreference to Medium'
+$ConfirmPreference = "Medium"
+
+Write-Output "`nWithout -WhatIf"
+Write-Output "Without Confirm"
+Write-Output "ConfirmPreference: $ConfirmPreference"
+Write-Output "ConfirmImpact: Medium"
+Test-WhatIfConfirm | Format-Table
+
+Write-Output "`n***************************`n"
+
+Write-Output "`nWithout -WhatIf"
+Write-Output 'With -Confirm:$false'
+Write-Output "ConfirmPreference: $ConfirmPreference"
+Write-Output "ConfirmImpact: Medium"
+Test-WhatIfConfirm -Confirm:$false | Format-Table
