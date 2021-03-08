36672f04d65fb722856eae04de3f04db50fab73d
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2021-03-07 20:50:05 -0500

    Update Collatz Conjecture files

--- 05MiscFiles/CollatzConjecture/CollatzConjecture.ps1
+++ 05MiscFiles/CollatzConjecture/CollatzConjecture.ps1
@@ -1,17 +1,36 @@
-# Collatz Conjecture Demo in PowerShell
+# Collatz Conjecture in PowerShell
 # Written By: Jeff Brusoe
-# Last Updated: December 3, 2020
+# Last Updated: March 7, 2021
 
 <#
 From Wikipedia - "The Collatz conjecture is a conjecture in mathematics that concerns a sequence 
 defined as follows: start with any positive integer n. Then each term is obtained from the previous
-term as follows: if the previous term is even, the next term is one half the previous term. If the 
-previous term is odd, the next term is 3 times the previous term plus 1. 
+term as follows: if the previous term is even, the next term is one half the previous term. If the
+previous term is odd, the next term is 3 times the previous term plus 1.
 The conjecture is that no matter what value of n, the sequence will always reach 1."
 #>
 
-[int]$Count = 0 #Keeps track of number of steps to get to 1
-[long]$i = Read-Host "Enter an integer greater than 1"
+$Count = 0 #Keeps track of number of steps to get to 1
+
+while ($true) {
+	#The purpose of this loop is to ensure that a proper starting value is entered.
+
+	try {
+		[ulong]$i = Read-Host "Enter an integer greater than or equal to 1" -ErrorAction Stop
+	}
+	catch {
+		Write-Warning "Please enter a valid starting integer"
+		continue
+	}
+
+	if ($i -lt 1) {
+		Write-Warning "Please enter a valid starting integer"
+	}
+	else {
+		break
+	}
+
+}
 
 Write-Output "Original Number: $i"
 
