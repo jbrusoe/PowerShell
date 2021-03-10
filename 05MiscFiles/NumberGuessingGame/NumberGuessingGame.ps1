5b9c99db8a09ac67df461e6898510b0b0709d59d
Jeff Brusoe <40579055+jbrusoe@users.noreply.github.com>
2021-03-10 18:16:21 -0500

    Update Number Guessing Game files

--- 05MiscFiles/NumberGuessingGame/NumberGuessingGame.ps1
+++ 05MiscFiles/NumberGuessingGame/NumberGuessingGame.ps1
@@ -1,18 +1,29 @@
 #PowerShell Number Guessing Game
+#Written by: Jeff Brusoe
+#Last Updated: March 10, 2021
 
-[int]$ActualNumber = Get-Random -minimum 1 -maximum 101
-[bool]$NumberGuessed = $false
-[int]$GuessCount = 0
+$ActualNumber = Get-Random -Minimum 1 -Maximum 101
+$NumberGuessed = $false
+$GuessCount = 0
 
 while (!$NumberGuessed)
 {
+	try {
+		[int]$UserGuess = Read-Host -Prompt "Guess number between 1 and 100" -ErrorAction Stop
+
+		if (($UserGuess -lt 1) -OR ($UserGuess -gt 100)) {
+			throw "Invalid number input"
+		}
+	}
+	catch {
+		Write-Warning "Please enter a valid integer between 1 and 100"
+		continue
+	}
+
 	$GuessCount++
 	Write-Output "`nNumber of Guesses: $GuessCount`n"
-	
-	[int]$UserGuess = Read-Host "Guess number between 1 and 100"
-	
-	if ($UserGuess -eq $ActualNumber)
-	{
+
+	if ($UserGuess -eq $ActualNumber) {
 		#Correct guess
 		Write-Host -ForeGroundColor Green "You are correct!"
 		
---
+++ 05MiscFiles/NumberGuessingGame/README.md
@@ -0,0 +1,3 @@
+## PowerShell - Number Guessing Game
+
+This is a PowerShell implementation of a number guessing game. The program randomly generates a number between 1 and 100 which the user has to guess. The program tells the user whether their guess is too high or too low until the correct value is entered.
